ed2fdb1d1dc496d414061b225a552f03ba71934d
Dhairya99999 <110965688+Dhairya99999@users.noreply.github.com>
2024-08-20 14:59:18 +0530

    Update googleDriveService.js

--- utils/googleDriveService.js
+++ utils/googleDriveService.js
@@ -7,14 +7,27 @@ import path from "path";
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
-
 const _dirname = dirname(__filename);
 
-const SERVICE_ACCOUNT_KEY_FILE = path.join(_dirname, "credForDocUpload.json");
+const credentials = {
+  type: 'service_account',
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+};
+
+
+// const SERVICE_ACCOUNT_KEY_FILE = path.join(_dirname, "credForDocUpload.json");
 const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: SERVICE_ACCOUNT_KEY_FILE,
+  credentials,
   scopes: SCOPES,
 });
 
